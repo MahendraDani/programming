e43e740e82cf6fd3b6ad1ce711bb02afcea61855
Mahendra Dani <danimahendra0904@gmail.com>
2025-09-10 09:38:59 +0530

    array problems

---
+++ sheets/apna-college/arrays/L540-subarray-sum-equals-k.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/
+// Date : 10.09.2025
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+// Time => O(N^2)
+int brute(vector<int>& arr, int k) {
+    int n = arr.size();
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int sum = 0;
+        for(int j=i;j<n;j++){
+            sum += arr[j];
+            if(sum==k) ans++;
+        }
+    }
+    return ans;
+}
+
+int better(vector<int>& arr, int k){
+    int n = arr.size();
+    vector<int> pref(n);
+    pref[0] = arr[0];
+
+    for(int i=1;i<n;i++){
+        pref[i] = arr[i] + pref[i-1];
+    }
+
+    map<int,int> mp;
+    int ans = 0;
+
+    for(int i=0;i<n;i++){
+        if(pref[i]==k) ans++;
+
+        if(mp.find(pref[i]-k)!=mp.end()){
+            ans += mp[pref[i]-k];
+        }
+
+        mp[pref[i]]++;
+    }
+    return ans;
+}
+
+int main(){
+  return 0;
+}
